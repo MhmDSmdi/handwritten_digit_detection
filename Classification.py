0691d7e1e36c7174a266bfbbceab932e1abbd469
MhmdSmdi <mhmd.samadi76@gmail.com>
2018-09-21 21:58:37 +0430

    TestImage add, prediction Complete

--- Classification.py
+++ Classification.py
@@ -1,12 +1,11 @@
-from sklearn.datasets import fetch_mldata
-from sklearn.model_selection import cross_val_predict
-from sklearn.linear_model import SGDClassifier
 import matplotlib
 import matplotlib.pyplot as plt
-from sklearn.model_selection import StratifiedKFold
-from sklearn.base import clone
 import numpy as np
 from PIL import Image
+from sklearn.base import clone
+from sklearn.datasets import fetch_mldata
+from sklearn.linear_model import SGDClassifier
+from sklearn.model_selection import StratifiedKFold
 
 
 def plotImageNumber(index):
@@ -50,6 +49,13 @@ def myCross_val_score(n_splits):
     return score
 
 
+def save_test_image():
+    for i in range(0, X_test.shape[0], 250):
+        test_img = X_test[i]
+        test_img_file = Image.fromarray(np.asarray(test_img.reshape(28, 28)), mode="L")
+        test_img_file.save("HandWritten_testImages/handWritten_" + str(i) + ".png")
+
+
 mnist = fetch_mldata('MNIST original')
 X, y = mnist["data"], mnist["target"]
 sgd_clf = SGDClassifier(random_state=42, max_iter=10)
@@ -57,15 +63,11 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 shuffle_index = np.random.permutation(60000)
 X_train, y_train = X_train[shuffle_index], y_train[shuffle_index]
 sgd_clf.fit(X_train, y_train)
+# save_test_image()
 
-# im = Image.open("newPIC0.png").convert("L").resize((28, 28))
-# im.save("newScaled.png")
-# img = list(im.getdata())
-# img_array = np.asarray(img)
-# img_array = img_array.reshape(28, 28)
-# # print(img_array)
-# # print(img_array.reshape(1, -1))
-# print("I guess the HandWrite Number is: " + str(sgd_clf.predict(img_array.reshape(1, -1))[0]))
-# plotImage(img_array)
-
-plotImageNumber(344)
+im = Image.open("HandWritten_testImages/handWritten_3000.png").convert("L").resize((28, 28))
+img = list(im.getdata())
+img_array = np.asarray(img)
+img_array = img_array.reshape(28, 28)
+print("I guess the HandWrite Number is: " + str(sgd_clf.predict(img_array.reshape(1, -1))[0]))
+plotImage(img_array)
